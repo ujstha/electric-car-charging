c7967a35ffe4d9a4406a77bf5d2c5da8494994ed
ujstha <ujstha27@gmail.com>
2019-12-19 19:10:28 +0200

    helmet remove

--- appMiddleware.js
+++ appMiddleware.js
@@ -1,10 +1,8 @@
-const helmet = require("helmet");
 const cors = require("cors");
 
 const appMiddleware = (express, app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(helmet());
   app.use(cors());
 };
 
